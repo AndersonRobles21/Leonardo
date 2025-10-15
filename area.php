de671b1476cc20d65e1c60bcc6e8c4176cfa55a3
Anderson <andersontopaga2018@gmail.com>
2025-10-14 20:28:07 -0500

    Add files via upload

---
+++ area.php
@@ -0,0 +1,18 @@
+<html>
+<style>
+body {
+    background-color: lightblue;
+}
+</style>
+<body>
+<?php
+print "El area del triangulo";
+$altura = 8;
+$base = 4;
+
+$area = $altura * $base;
+
+echo "Teniendo en cuenta que la altura es de " . $altura . " y su base es de " . $base . " su área es de " . $area;
+?>
+</body>
+</html>
